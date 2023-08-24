33d1c8b7d8b60cc68b3ee39ff3aed39ecae448f1
Ali Renan Uyanık <renanuya@gmail.com>
2023-08-24 05:31:42 +0300

    auth settings

--- lib/feature/auth/authentication_view.dart
+++ lib/feature/auth/authentication_view.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart' as firebase;
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kartal/kartal.dart';
 
 class AuthenticationVeiw extends ConsumerStatefulWidget {
   const AuthenticationVeiw({super.key});
@@ -15,10 +16,29 @@ class _AuthenticationVeiwState extends ConsumerState<AuthenticationVeiw> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: firebase.LoginView(
-        action: firebase.AuthAction.signIn,
-        providers:
-            firebase.FirebaseUIAuth.providersFor(FirebaseAuth.instance.app),
+      appBar: AppBar(
+        title: const Text('Admin Giriş Paneli'),
+      ),
+      body: Theme(
+        data: context.general.appTheme.copyWith(
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+        child: Center(
+          //TODO: Container ile çevir
+          child: Padding(
+            padding: context.padding.medium,
+            child: firebase.LoginView(
+              showTitle: false,
+              action: firebase.AuthAction.signIn,
+              providers: firebase.FirebaseUIAuth.providersFor(
+                  FirebaseAuth.instance.app),
+            ),
+          ),
+        ),
       ),
     );
   }
